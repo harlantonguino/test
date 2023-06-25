1b48b64556b2a4959ad80565073786b4b41735a0
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 <harlan.tonguino@hotmail.com>
2023-06-24 23:41:20 -0500

    v0.2

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ df['productionCountry'].fillna(value='No Data', inplace=True)
 df['cast'].fillna(value='No Data', inplace=True)
 df['director'].fillna(value='No Data', inplace=True)
 
-###########
+
 
 from fastapi import FastAPI
 
@@ -34,51 +34,60 @@ app = FastAPI()
 
 @app.get('/')        
 def welcome():
-    return {'mensaje': 'hola'}
-
-#######
+    return {'mensaje': 'Hola'}
 
 @app.get('/cantidad_filmaciones_mes/{mes}')
 def cantidad_filmaciones_mes(mes:str):
+    dict_mes={'enero':1, 'febrero':2, 'marzo':3, 'abril':4, 'mayo':5, 'junio':6, 'julio':7, 'agosto':8, 'septiembre':9, 'octubre':10, 'noviembre':11, 'diciembre':12}
+    if dict_mes.get(mes.lower(), 0) > 0:
+        nmes=dict_mes.get(mes.lower())
+        respuesta=(df['releaseDate'].dt.month==nmes).sum().item()
+        return {'mes':mes.capitalize(), 'cantidad':respuesta}
+    else:
+        return {'¡mes no valido!':'ingresa un mes valido, ejemplo: enero'}
 
-    lista=['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
-    nmes=lista.index(mes)+1
-    respuesta=(df['releaseDate'].dt.month==nmes).sum().astype(str)
-    
-    return {'mes':mes, 'cantidad':respuesta}
-
-@app.get('/cantidad_filmaciones_dia/{dia}')
+@app.get('/cantidad_filmaciones_dia{dia}')
 def cantidad_filmaciones_dia(dia:str):
 
-    list_dia=['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
-    ndia=list_dia.index(dia)
-    respuesta=(df['releaseDate'].dt.dayofweek==ndia).sum().astype(str)
-    
-    return {'dia':dia, 'cantidad':respuesta}
+    dict_dia={'lunes':0, 'martes':1, 'miercoles':2, 'jueves':3, 'vierne':4, 'sabado':5, 'domingo':6}
+
+    if dict_dia.get(dia.lower(), -1) >= 0:
+        ndia=dict_dia.get(dia.lower())
+        respuesta=(df['releaseDate'].dt.dayofweek==ndia).sum().item()
+        return {'dia':dia.capitalize(), 'cantidad':respuesta}
+    else:
+        return {'¡dia no valido!':'ingresa un dia valido, ejemplo: lunes'}
 
 @app.get('/score_titulo/{titulo}')
 def score_titulo(titulo:str):
 
-    funcion_3 = df[df['title']==titulo][['releaseYear', 'score']]
-    funcion_3.reset_index(drop=True, inplace=True)
-    respuesta_1 = funcion_3['releaseYear'][0].astype(str)
-    respuesta_2 = funcion_3['score'][0].astype(str)
-    
-    return {'titulo':titulo, 'anio':respuesta_1, 'popularidad':respuesta_2}
+   if len(df[df['title']==titulo.title()]) > 0:
+         
+         funcion_3 = df[df['title']==titulo.title()][['releaseYear', 'score']]
+         funcion_3.reset_index(drop=True, inplace=True)
+         respuesta_1 = funcion_3['releaseYear'][0].item()
+         respuesta_2 = funcion_3['score'][0].item()
+        
+         return {'titulo':titulo.title(), 'anio':respuesta_1, 'popularidad':respuesta_2}
+   else:
+         return {'¡titulo no valido!':'ingresa un titulo valido, ejemplo: Toy Story'}
 
 @app.get('/votos_titulo/{titulo}')
 def votos_titulo(titulo:str):
     
-    funcion_4 = df[df['title']==titulo][['releaseYear', 'votes', 'rating']]
-    funcion_4.reset_index(drop=True, inplace=True)
-    respuesta_1 = funcion_4['releaseYear'][0].astype(str)
-    respuesta_2 = funcion_4['votes'][0].astype(str)
-    respuesta_3 = funcion_4['rating'][0].astype(str)
+    if len(df[df['title']==titulo.title()]) > 0:
+        funcion_4 = df[df['title']==titulo.title()][['releaseYear', 'votes', 'rating']]
+        funcion_4.reset_index(drop=True, inplace=True)
+        respuesta_1 = funcion_4['releaseYear'][0].item()
+        respuesta_2 = funcion_4['votes'][0].item()
+        respuesta_3 = funcion_4['rating'][0].item()
 
-    if funcion_4['votes'][0] >= 2000:
-        return {'titulo':titulo, 'anio':respuesta_1, 'voto_total':respuesta_2, 'voto_promedio':respuesta_3}
+        if funcion_4['votes'][0] >= 2000:
+            return {'titulo':titulo.title(), 'anio':respuesta_1, 'voto_total':respuesta_2, 'voto_promedio':respuesta_3}
+        else:
+            return {'mensaje':'No cumple con votacion igual o mayor a 2000 votos'}
     else:
-        return {'mensaje':'No cumple con votacion igual o mayor a 2000 votos'}  
+        return {'¡titulo no valido!':'ingresa un titulo valido, ejemplo: Toy Story'}   
     
 @app.get('/get_actor/{nombre_actor}')
 def get_actor(nombre_actor:str):
@@ -93,10 +102,12 @@ def get_actor(nombre_actor:str):
 
 @app.get('/get_director/{nombre_director}')
 def get_director(nombre_director:str):
+     
+    nombre_director=nombre_director.title()
 
     funcion_6 = df[df['director'].str.contains(nombre_director)][['id', 'title', 'releaseYear', 'budget', 'revenue', 'return']]
     funcion_6.reset_index(drop=True, inplace=True) 
-    respuesta_1 = funcion_6['return'].sum()
+    respuesta_1 = round(funcion_6['return'].sum(), 2)
     respuesta_2 = funcion_6['title'].tolist()
     respuesta_3 = funcion_6['releaseYear'].tolist()
     respuesta_4 = funcion_6['return'].tolist()
@@ -111,4 +122,4 @@ def get_director(nombre_director:str):
         j=indice.index(i)
         pelis.append({'titulo': respuesta_2[j], 'anio':respuesta_3[j], 'retorno_pelicula':respuesta_4[j], 'budget_pelicula':respuesta_5[j], 'revenue_pelicula':respuesta_6[j]})
 
-    return {'director':nombre_director, 'retorno_total_director':respuesta_1, 'peliculas': pelis}
+    return {'director':nombre_director.title(), 'retorno_total_director':respuesta_1, 'peliculas': pelis}
